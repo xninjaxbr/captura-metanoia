31b1c8d2d288aaeebda54912cfd204b82078c778
Paulo Henrique <p.henrique_87@hotmail.com>
2025-07-26 09:28:00 -0300

    Projeto Metanoia 1.4.3 teste no db.js prisma

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,8 +1,9 @@
-import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
+
+
 neonConfig.webSocketConstructor = ws;
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 neonConfig.poolQueryViaFetch = true
